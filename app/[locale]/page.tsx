72592d255120b978320902619c7cce173ec936dd
Théo Gautier <gotchit@outlook.fr>
2025-08-03 11:23:31 +0200

    wip i18n

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -28,18 +28,24 @@ export default function Home() {
               <div className='px-8 ml-0 md:ml-36 z-1 flex flex-col items-start gap-12 relative'>
                   <h2
                       className="text-[2.3rem] lg:text-5xl">
-                      Une technologie IRM <br/><span className='font-semibold'>à la pointe <br/>de l'innovation.</span>
+                      {t.rich('ia-section-title', {
+                          semibold: (chunks) =>  <><br/><span className='font-semibold'>{chunks}</span></>
+                      })}
                   </h2>
                   <div className='flex flex-col gap-8'>
                       <div className='flex items-center gap-4'>
                           <Image width={75} height={75} src="/radiation.svg" alt=""/>
                           <p className='text-[1.5rem]'>
-                              <span className='font-bold'>Sans</span> Radiation
+                              {t.rich('ia-section-p1', {
+                                  semibold: (chunks) =>  <span className='font-semibold'>{chunks}</span>
+                              })}
                           </p>
                       </div>
                       <div className='flex items-center gap-4'><Image width={75} height={75} src="/irm.svg" alt=""/>
                           <p className='text-[1.5rem]'>
-                              <span className='font-bold'>6000 à 7000</span> images
+                              {t.rich('ia-section-p2', {
+                                  semibold: (chunks) =>  <span className='font-semibold'>{chunks}</span>
+                              })}
                           </p>
                       </div>
                   </div>
@@ -49,24 +55,27 @@ export default function Home() {
           <Section id='book-a-scan'>
               <h2
                   className="inline-block max-w-xl text-center justify-center tracking-tight inline text-[2.3rem] lg:text-5xl mb-8 z-1">
-                  Planifiez votre rdv avec notre concierge <span
-                  className='font-semibold'>rdv avec notre concierge</span> afin d’en savoir plus.
+                  {t.rich('book-a-scan-section-title', {
+                      semibold: (chunks) =>  <span className='font-semibold'>{chunks}</span>
+                  })}
               </h2>
 
               <InscriptionForm/>
           </Section>
 
           <section className="flex flex-col items-center justify-center py-8 min-h-screen relative mb-8 p-8">
-              <h1 className="inline-block max-w-xl text-center justify-center tracking-tight inline text-[2.3rem] lg:text-5xl z-1 mb-16">
-                  Réservez votre examen <span className='font-semibold'>Revoscan</span>
-              </h1>
+              <h2 className="inline-block max-w-xl text-center justify-center tracking-tight inline text-[2.3rem] lg:text-5xl z-1 mb-16">
+                  {t.rich('price-section-title', {
+                      semibold: (chunks) =>  <span className='font-semibold'>{chunks}</span>
+                  })}
+              </h2>
 
               <div className='w-2/3 flex flex-col md:flex-row gap-8 justify-around z-1 mb-8'>
-                  <p className='text-2xl'><span className='font-semibold'>Prix de l'examen</span><br/>4 500 CHF</p>
-                  <p className='text-2xl'><span className='font-semibold'>Durée</span><br/>50 minutes</p>
+                  <p className='text-2xl'><span className='font-semibold'>{t('price-section-price-title')}</span><br/>{t('price-section-price')}</p>
+                  <p className='text-2xl'><span className='font-semibold'>{t('price-section-duration-title')}</span><br/>{t('price-section-duration')}</p>
               </div>
 
-              <ButtonLink href='/public#book-a-scan' variant='transparent'>Réserver un scan</ButtonLink>
+              <ButtonLink href='/#book-a-scan' variant='transparent'>{t('price-section-call-to-action')}</ButtonLink>
 
               <Image src="/1.webp" fill={true} alt=""
                      className='absolute w-full h-full object-cover mask-y-from-70% mask-y-to-90% opacity-45'/>
@@ -75,7 +84,9 @@ export default function Home() {
           <Section bgSrc='/3.webp' bgOpacity='opacity-50'
                    className="flex flex-col text-center items-center justify-center py-8 min-h-screen relative mb-8 px-8">
               <h2 className="mb-8 text-[2.3rem] lg:text-5xl z-1">
-                  Associée à l’expertise <br/> <span className="font-semibold">de nos médecins renommés.</span>
+                  {t.rich('carousel-section-title', {
+                      semibold: (chunks) =>  <><br/><span className='font-semibold'>{chunks}</span></>
+                  })}
               </h2>
 
               <CarouselMedecins/>
@@ -83,7 +94,9 @@ export default function Home() {
 
           <Section bgSrc={"/ai-bg.webp"} bgOpacity="opacity-75">
               <h2 className="px-8 text-center text-[2.3rem] lg:text-5xl z-1">
-                  Et à la puissance de <span className='font-semibold'>l'intelligence artificielle.</span>
+                  {t.rich('ai-power-section-title', {
+                      semibold: (chunks) =>  <><br/><span className='font-semibold'>{chunks}</span></>
+                  })}
               </h2>
           </Section>
 
